3a80ad72dc2a6d935de8e772f874bc48bafbeb0b
koren850 <korenah3@gmail.com>
2022-03-01 20:30:34 +0200

    Fixed heroku

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 var config;
 
 // keys.js - figure out what set of credentials to return
-if (false && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   console.log('In production');
   config = require('./prod')
 } else {
   console.log('In development');
   // we are in development - return the dev keys!!!
-  // config = require('./dev')
+  config = require('./dev')
 }
 
 module.exports = config
